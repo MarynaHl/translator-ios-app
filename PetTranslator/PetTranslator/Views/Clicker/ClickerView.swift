9c02ef783577050ac5fe309ea7ccf6a552894770
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-02-20 19:04:58 +0000

    add-settings-list-in-clicker-view

--- PetTranslator/PetTranslator/Views/Clicker/ClickerView.swift
+++ PetTranslator/PetTranslator/Views/Clicker/ClickerView.swift
@@ -2,9 +2,87 @@ import SwiftUI
 
 struct ClickerView: View {
     var body: some View {
-        Text("Clicker Screen")
-            .font(.title)
-            .foregroundColor(.green)
+        NavigationView {
+            List {
+                Section {
+                    Button(action: {
+                        rateApp()
+                    }) {
+                        Text("Rate Us")
+                    }
+                    
+                    Button(action: {
+                        shareApp()
+                    }) {
+                        Text("Share App")
+                    }
+                    
+                    Button(action: {
+                        contactUs()
+                    }) {
+                        Text("Contact Us")
+                    }
+                }
+                
+                Section {
+                    Button(action: {
+                        restorePurchases()
+                    }) {
+                        Text("Restore Purchases")
+                    }
+                }
+                
+                Section {
+                    Button(action: {
+                        openPrivacyPolicy()
+                    }) {
+                        Text("Privacy Policy")
+                    }
+                    
+                    Button(action: {
+                        openTermsOfUse()
+                    }) {
+                        Text("Terms of Use")
+                    }
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Settings")
+        }
+    }
+    
+    func rateApp() {
+        print("Rate app tapped")
+    }
+    
+    func shareApp() {
+        let textToShare = "Check out this awesome Pet Translator app!"
+        let activityVC = UIActivityViewController(activityItems: [textToShare], applicationActivities: nil)
+        
+        if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootVC = scene.windows.first?.rootViewController {
+            rootVC.present(activityVC, animated: true, completion: nil)
+        }
+    }
+    
+    func contactUs() {
+        print("Contact us tapped")
+    }
+    
+    func restorePurchases() {
+        print("Restore purchases tapped")
+    }
+    
+    func openPrivacyPolicy() {
+        if let url = URL(string: "https://yourPrivacyPolicyLink.com") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    func openTermsOfUse() {
+        if let url = URL(string: "https://yourTermsOfUseLink.com") {
+            UIApplication.shared.open(url)
+        }
     }
 }
 
