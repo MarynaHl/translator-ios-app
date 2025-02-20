b5823dcbaa3a7545d1295151e4c583ee5642f9d7
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-02-20 21:11:11 +0000

    main-view-setup-and-navigation-fix

--- PetTranslator/PetTranslator/PetTranslatorApp.swift
+++ PetTranslator/PetTranslator/PetTranslatorApp.swift
@@ -1,17 +1,10 @@
-//
-//  PetTranslatorApp.swift
-//  PetTranslator
-//
-//  Created by Maryna on 20/02/2025.
-//
-
 import SwiftUI
 
 @main
 struct PetTranslatorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
