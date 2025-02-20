be08286ef7cc723ee87b0bb9e322d5f0e7096c35
Maryna Hlushak <75501308+MarynaHl@users.noreply.github.com>
2025-02-20 14:47:20 +0000

    init-project-structure

---
+++ PetTranslator/PetTranslator/Views/ContentView.swift
@@ -0,0 +1,11 @@
+// PetTranslatorApp.swift
+import SwiftUI
+
+@main
+struct PetTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() // Почнемо з головного в'ю
+        }
+    }
+}
---
+++ PetTranslator/PetTranslatorTests/PetTranslatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetTranslatorTests.swift
+//  PetTranslatorTests
+//
+//  Created by Maryna on 20/02/2025.
+//
+
+import Testing
+@testable import PetTranslator
+
+struct PetTranslatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
